95411034a219557f04e664b462fc176ac1336181
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-04-08 23:34:34 +0200

    Admin Template

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('admin.dashboard');
+        return view('admin.index');
     }
 }
